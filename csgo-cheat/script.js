8a1a1bd49f6215b133c935b9ee744392f805b89e
h1lar1ous <97308069+h1lar1ous@users.noreply.github.com>
2022-01-26 21:09:46 +0400

    Add files via upload

---
+++ csgo-cheat/script.js
@@ -0,0 +1,298 @@
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".nav-link"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 992){
+      elem.addClass('text-center');
+    }
+    else{
+      elem.removeClass('text-center');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".pers"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 865){
+      elem.removeClass('col-6 d-flex ms-auto');
+    }
+    else{
+      elem.addClass('col-6 d-flex ms-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".main"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 865){
+      elem.addClass('col-12 mx-auto');
+    }
+    else{
+      elem.removeClass('col-12 mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+  
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".main-text"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 865){
+      elem.addClass('text-center');
+    }
+    else{
+      elem.removeClass('text-center');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+  
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".main-ps-text"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 865){
+      elem.addClass('text-center');
+    }
+    else{
+      elem.removeClass('text-center');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".buy-btn"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 865){
+      elem.addClass('d-block mx-auto');
+    }
+    else{
+      elem.removeClass('d-block mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".more-btn"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 1284){
+      elem.addClass('d-block mx-auto');
+    }
+    else{
+      elem.removeClass('d-block mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".third"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 1043){
+      elem.addClass('mx-auto');
+    }
+    else{
+      elem.removeClass('mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".first"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 696){
+      elem.addClass('mx-auto');
+    }
+    else{
+      elem.removeClass('mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".second"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 696){
+      elem.addClass('mx-auto');
+    }
+    else{
+      elem.removeClass('mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".sixth"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 1043){
+      elem.addClass('mx-auto');
+    }
+    else{
+      elem.removeClass('mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".fourth"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 696){
+      elem.addClass('mx-auto');
+    }
+    else{
+      elem.removeClass('mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".fifth"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 1043){
+      elem.removeClass('mx-auto');
+      elem.addClass('ms-auto');
+    }
+    else{
+      elem.removeClass('ms-auto');
+      elem.addClass('mx-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
+
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".fifth"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 696){
+      elem.removeClass('ms-auto');
+      elem.addClass('mx-auto');
+    }
+    else{
+      elem.addClass('ms-auto');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
